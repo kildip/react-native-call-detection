ae1519ef19763af0991f134cc9fadf422af4f1da
Sendy Akbar <sendy.akbar@myboost.id>
2021-06-21 18:06:51 +0700

    solve undefined source

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ BatchedBridge.registerCallableModule('CallStateUpdateActionModule', CallStateUpd
 
 // https://stackoverflow.com/questions/13154445/how-to-get-phone-number-from-an-incoming-call : Amjad Alwareh's answer.
 const requestPermissionsAndroid = (permissionMessage) => {
-  const requiredPermission = Platform.constants.Release >= 9
+  const requiredPermission = Platform.OS === 'android'
     ? PermissionsAndroid.PERMISSIONS.READ_CALL_LOG
     : PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE
   return PermissionsAndroid.check(requiredPermission)
